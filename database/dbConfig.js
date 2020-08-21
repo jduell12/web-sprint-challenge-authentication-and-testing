1dfba016123e97b8f4c178eab08f43219eedeb43
Jessica <duellje@oregonstate.edu>
2020-08-21 12:44:10 -0400

    added testing db for tests

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,4 +3,4 @@ const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 const environment = process.env.DB_ENV || "development";
 
-module.exports = knex(knexConfig[environemnt]);
+module.exports = knex(knexConfig[environment]);
