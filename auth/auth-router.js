5602586975ce87ac6cc6b6a10d7bb4ef56e9c7ad
Jessica <duellje@oregonstate.edu>
2020-08-21 11:23:29 -0400

    added register function to auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,35 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const Users = require("../users/usersModel");
+const { isValid, loginValid } = require("../users/usersService");
+const constants = require("../config/constants");
+
+router.post("/register", (req, res) => {
+  const user = req.body;
+
+  if (isValid(user)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 12;
+
+    //hash password
+    const hash = bcryptjs.hashSync(user.password, rounds);
+    user.password = hash;
+
+    //add user to the database
+    Users.addUser(user)
+      .then((user) => {
+        res.status(201).json({ data: user });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  } else {
+    res.status(400).json({ message: "please provide a username and password" });
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
