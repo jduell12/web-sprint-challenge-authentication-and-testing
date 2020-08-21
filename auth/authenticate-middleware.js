4351136bbd169ea0848a561e8eb45435bb5540a3
Jessica <duellje@oregonstate.edu>
2020-08-21 11:11:05 -0400

    added config file, completed auth middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,21 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
+const constants = require("../config/constants");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, constants.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ you: "shall not pass!" });
+      } else {
+        req.jwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: "shall not pass!" });
+  }
 };
